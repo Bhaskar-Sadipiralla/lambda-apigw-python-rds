00f160f15caf069e0e06c3cf0dbfa1eb9b2e6dc8
prasadandroiddeveloper@gmail.com <sbhaskar.sreddy@gmail.com>
2021-10-17 11:34:12 +0530

    updated

---
+++ rds_config.py
@@ -0,0 +1,5 @@
+ #config file containing credentials for RDS MySQL instance
+db_username = "lambdauser"
+db_password = "NHSn7mzLtz6v"
+db_name = "employee_db" 
+db_host = "lambda-demo.cvronvarhmik.ap-southeast-1.rds.amazonaws.com"
